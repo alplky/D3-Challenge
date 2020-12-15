9b351bb4c88ea042d8e7431d13476e0c34b4b618
alplky <alicia.pelkey@gmail.com>
2020-12-15 15:16:43 -0600

    data transitioning between selections, need to figure out fill and text

--- bonus/assets/js/app.js
+++ bonus/assets/js/app.js
@@ -1,6 +1,6 @@
 // set up entire svg area
-const svgHeight = 800;
-const svgWidth = 1000;
+const svgHeight = 600;
+const svgWidth = 800;
 
 const margin = {
     top: 100, 
@@ -21,6 +21,49 @@ const svg = d3.select("#scatter").append("svg")
 const chartG = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`)
     .classed("chart-group", true);
+
+const xScale = (data, selection) => {
+
+    let selectionData
+    if (selection === "In Poverty (%)"){
+        selectionData = data.map(d => parseFloat(d.poverty))
+    } else if (selection === "Income (Median)"){
+        selectionData = data.map(d => parseFloat(d.income))
+    } else if (selection === "Age (Median)"){
+        selectionData = data.map(d => parseFloat(d.age))
+    }
+    console.log(selectionData)
+          
+    const x = d3.scaleLinear()
+        .domain([0, d3.max(selectionData)])
+        .range([0, chartWidth])
+          
+        return(x)
+}
+          
+const renderXAxis = (xAxisG, newXScale) => {
+    xAxis = d3.axisBottom(newXScale)
+    xAxisG.transition()
+        .duration(1000)
+        .call(xAxis)
+}
+          
+const renderCircles = (circles, newXScale, selection) =>{
+          
+    let selectionDataKey
+          
+    if (selection === "In Poverty (%)"){
+        selectionDataKey = "poverty"
+    } else if (selection === "Income (Median)"){
+        selectionDataKey = "income"
+    } else if (selection === "Age (Median)"){
+        selectionDataKey = "age"
+    }
+          
+    circles.transition()
+        .duration(1000)
+        .attr("cx", d => newXScale(d[selectionDataKey]))
+}   
 
 // read in data for scatter plot
 d3.csv("assets/data/data.csv").then(data => {
@@ -44,13 +87,18 @@ d3.csv("assets/data/data.csv").then(data => {
     chartG.append("g")
         .call(yAxis);
 
-    chartG.append("g")
+    // chartG.append("g")
+    //     .attr("transform", `translate(0, ${chartHeight})`)
+    //     .call(xAxis);
+
+    const xAxisG = chartG.append("g")
         .attr("transform", `translate(0, ${chartHeight})`)
-        .call(xAxis);
+        .call(xAxis)
 
     // create label areas and add text
+    // X labels ---------------------------------
     const xLabelArea = svg.append("g")
-        .attr("transform", `translate(${svgWidth - 625}, ${svgHeight - margin.bottom + 45})`);
+        .attr("transform", `translate(${svgWidth - 575}, ${svgHeight - margin.bottom + 45})`);
 
     xLabelArea.append("text")
         .attr("stroke", "#000000")
@@ -66,8 +114,18 @@ d3.csv("assets/data/data.csv").then(data => {
         .text("Age (Median)")
         .attr("dy", "40");
 
+    xLabelArea.selectAll("text")
+        .on("click", function() {
+          const selection = d3.select(this).text()
+          console.log(selection)
+          newXScale = xScale(data, selection)
+          renderXAxis(xAxisG, newXScale)
+          renderCircles(circles, newXScale, selection) 
+        })
+
+    // Y labels ---------------------------------
     const yLabelArea = svg.append("g")
-        .attr("transform", `translate(${svgWidth - margin.left - 830}, ${svgHeight - 250})`);
+        .attr("transform", `translate(${svgWidth - margin.left - 630}, ${svgHeight - 250})`);
 
     yLabelArea.append("text")
         .attr("transform", "rotate(-90)")
@@ -78,25 +136,34 @@ d3.csv("assets/data/data.csv").then(data => {
     plotArea = chartG.append("g")
         .classed("plot-area", true)
 
-    // bind data to groups for x and y data points
-    circleG = plotArea.selectAll("g")
+    // // bind data to groups for x and y data points
+    // const circleG = plotArea.selectAll("g")
+    //     .data(data)
+    //     .enter()
+    //     .append("g")
+    //     .attr("transform", d => `translate(${x(parseFloat(d.poverty))}, ${y(parseFloat(d.healthcare))})`)
+
+    // // append circles, set radius, and fill
+    // circleG.append("circle")
+    //     .attr("r", 13)
+    //     .attr("stroke-width", 2)
+    //     .attr("fill", "rgb(89, 124, 158)")
+
+    // // add state abbreviations on top of circles
+    // circleG.append("text")
+    //     .text(d => d.abbr)
+    //     .attr("stroke", "rgb(255, 255, 255)")
+    //     .attr("fill", "rgb(255, 255, 255)")
+    //     .attr("dy", ".3em")
+    //     .attr("text-anchor", "middle")
+
+    const circles = chartG.selectAll("circle")
         .data(data)
         .enter()
-        .append("g")
-        .attr("transform", d => `translate(${x(parseFloat(d.poverty))}, ${y(parseFloat(d.healthcare))})`)
+        .append("circle")
+        .attr("cx", d => parseFloat(x(d.poverty)))
+        .attr("cy", d => parseFloat(y(d.healthcare)))
+        .attr("r", 10)
 
-    // append circles, set radius, and fill
-    circleG.append("circle")
-        .attr("r", 13)
-        .attr("stroke-width", 2)
-        .attr("fill", "rgb(89, 124, 158)")
-
-    // add state abbreviations on top of circles
-    circleG.append("text")
-        .text(d => d.abbr)
-        .attr("stroke", "rgb(255, 255, 255)")
-        .attr("fill", "rgb(255, 255, 255)")
-        .attr("dy", ".3em")
-        .attr("text-anchor", "middle")
 
     });
