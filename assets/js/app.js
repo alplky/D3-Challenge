ebfb142a33079695ba8a822a683868832c77ab10
alplky <alicia.pelkey@gmail.com>
2020-12-15 10:39:06 -0600

    created scales and axes displaying on page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 // set up entire svg area
-const svgHeight = 400;
-const svgWidth = 800;
+const svgHeight = 600;
+const svgWidth = 1200;
 
 const margin = {
     top: 50, 
@@ -25,4 +25,29 @@ const chartG = svg.append("g")
 // read in data for scatter plot
 d3.csv("assets/data/data.csv").then(data => {
     console.log(data)
+
+    // establish y value
+    const y = d3.scaleLinear()
+        .domain([0, d3.max(data.map(d => parseFloat(d.healthcare)))])
+        .range([chartHeight, 0])
+
+    // establish x value
+    const x = d3.scaleLinear()
+        .domain([0, d3.max(data.map(d => parseFloat(d.poverty)))])
+        .range([0, chartWidth])
+
+    // create axes values
+    const yAxis = d3.axisLeft(y)
+    const xAxis = d3.axisBottom(x)
+
+    // put axes on the svg
+    chartG.append("g")
+        .call(yAxis)
+
+    chartG.append("g")
+        .attr("transform", `translate(0, ${chartHeight})`)
+        .call(xAxis)
+
+    
+
 })
